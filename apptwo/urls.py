eca78014a560b439a7830fa4d9f4747f408042c0
fridayshoes <one_amp@hotmail.com>
2026-03-03 10:08:54 +0000

    added url endpoints for apptwo

--- apptwo/urls.py
+++ apptwo/urls.py
@@ -4,3 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+
+
+urlpatterns = [
+    path('apptwoapi/endpoint1/', views.app_two_view1, name='app-two-view1'),
+    path('apptwoapi/endpoint2/', views.app_two_view2, name='app-two-view2'),
+    path('apptwoapi/endpoint3/', views.app_two_view3, name='app-two-view3'),
+]
--- apptwo/views.py
+++ apptwo/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse  # <--- Add this import
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This is the home page for apptwo!")
+    return HttpResponse("Hello, World! This is the home page of apptwo.")
+
+def app_two_view1(request):
+    return HttpResponse("This is the first view for apptwo!")
+
+def app_two_view2(request):
+    return HttpResponse("This is the second view for apptwo!")
+
+def app_two_view3(request):
+    return HttpResponse("This is the third view for apptwo!")
